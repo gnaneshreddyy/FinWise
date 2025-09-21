036a2ade64d8b3ce09787b17033c245c71375587
gnaneshreddyy <gnaneshreddyy@gmail.com>
2025-09-21 19:33:54 +0530

    with chatbot implementation

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Insights from './components/Insights'
+import Chatbot from './components/Chatbot'
 
 export default function App() {
   return (
     <>
     
     <Home/>
-    <Insights/>
+  
+   <Chatbot/>
     </>
   )
 }
