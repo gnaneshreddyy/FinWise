9193602d075cf630a66ce0aec2a0105710aea030
gnaneshreddyy <gnaneshreddyy@gmail.com>
2025-09-21 20:16:57 +0530

    fixed smthing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
-import Insights from './components/Insights'
 import Chatbot from './components/Chatbot'
 
 export default function App() {
